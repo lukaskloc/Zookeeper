c1f69b324f8aca66071bc1ad4458865286e05be0
Łukasz Kloc <lukaskloc@Mac-mini-ukasz.local>
2023-03-12 15:29:25 +0100

    Initial commit

---
+++ Topics/The while and do-while loops/Squares of natural numbers/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // start coding here
+        int maxLimit = scanner.nextInt();
+
+        int i = 1;
+
+        while (i * i <= maxLimit) {
+            System.out.println(i * i);
+            i++;
+        }
+    }
+}
